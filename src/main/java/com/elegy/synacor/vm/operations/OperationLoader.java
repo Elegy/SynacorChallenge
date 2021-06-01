286aed99a6482f1893dca69d67690aa6cab952ac
Nathan Smith <nathan@Nathans-MacBook-Air.local>
2021-06-01 16:19:08 -0700

    Log unsupported opcodes to help prioritize self-test implementation

--- src/main/java/com/elegy/synacor/vm/operations/OperationLoader.java
+++ src/main/java/com/elegy/synacor/vm/operations/OperationLoader.java
@@ -15,11 +15,14 @@ public class OperationLoader {
     public Operation load(int address) {
         int opcode = ram.read(address);
         switch (opcode) {
+            case 0:
+                return null;
             case 19:
                 return new Out(address, ram, registers);
             case 21:
                 return new Noop(address, ram, registers);
             default:
+                System.out.println("Unsupported operation with opcode " + opcode);
                 return null;
         }
     }
